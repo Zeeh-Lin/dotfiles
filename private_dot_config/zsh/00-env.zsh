f8611828fa2a5158d87a282308ea5e80e7c47745
Zeeh-Lin <144679645+Zeeh-Lin@users.noreply.github.com>
2026-07-08 19:57:20 +0800

    Updates.

--- private_dot_config/zsh/00-env.zsh
+++ private_dot_config/zsh/00-env.zsh
@@ -6,8 +6,14 @@ export PATH="$PATH:/opt/nvim"
 export HISTFILE="${XDG_DATA_HOME}/zsh/history"
 export _ZL_DATA="${XDG_DATA_HOME}/zlua/.zlua"
 
+# TOOLS PATH
+export NVM_DIR="${XDG_DATA_HOME}/nvm"
+export CC_SWITCH_CONFIG_DIR="${XDG_CONFIG_HOME}/cc-switch"
+export CLAUDE_CONFIG_DIR="${XDG_CONFIG_HOME}/claude"
+export CODEX_HOME="${XDG_CONFIG_HOME}/codex"
+export KIMI_CODE_HOME="${XDG_CONFIG_HOME}/kimi-code"
+
 setopt auto_cd
 setopt extended_glob
 setopt nomatch
 setopt interactive_comments
-setopt correct
--- private_dot_config/zsh/50-functions.zsh
+++ private_dot_config/zsh/50-functions.zsh
@@ -5,3 +5,12 @@ fpath+=("${ZDOTDIR}/functions")
 
 # Initialize z.lua (plugin loaded in 40-plugins.zsh)
 eval "$(lua ${ZINIT_HOME:h}/plugins/skywind3000---z.lua/z.lua --init zsh)"
+
+# nvm configurations
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# cc-switch completions
+if [ -d "$XDG_DATA_HOME/zsh/site-functions" ]; then
+  fpath=("$XDG_DATA_HOME/zsh/site-functions" $fpath)
+fi
